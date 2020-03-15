86bf948115c2ef04d3c81b09b769980f0f3942c0
sano fer <sano_fer@yahoo.com>
2020-03-15 13:40:25 -0400

    First commit

---
+++ target/generated-sources/annotations/com/revature/ers/user.java
@@ -0,0 +1,44 @@
+package com.revature.ers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class user
+ */
+public class user extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public user() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("My User:").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.print("<html><body><h2>This is from my user</h2></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
